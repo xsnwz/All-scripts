bb93d56290f5ceae71b0e3754644576960339f63
tobiee <sourced449@gmail.com>
2025-05-02 14:16:48 +0700

    Update test.lua

--- Libraries/test.lua
+++ Libraries/test.lua
@@ -5206,6 +5206,7 @@ local aa = {
             ElementBorder = Color3.fromRGB(25, 25, 25),
             InElementBorder = Color3.fromRGB(55, 55, 55),
             ElementTransparency = 0.82,
+            ToggleToggled = Color3.fromRGB(0, 0, 0),
             DropdownFrame = Color3.fromRGB(120, 120, 120),
             DropdownHolder = Color3.fromRGB(35, 35, 35),
             DropdownBorder = Color3.fromRGB(25, 25, 25),
